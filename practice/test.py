6e0f5124041e542ff435b3561783ff48daa4b75c
DuncanRMcKeith <drmckeith@email.neit.edu>
2025-01-07 08:40:08 -0500

    Update test.py

--- practice/test.py
+++ practice/test.py
@@ -1,3 +1,4 @@
 #This is a test server for all SE126 Work, This file will only be to test our work for VSC and GitHub
 #This test code is another test to show ive properlly pulled it.
 #Here is an even newer line of code to show i can properlly pull it
+print ("This is how you would do a print statment everyone")
