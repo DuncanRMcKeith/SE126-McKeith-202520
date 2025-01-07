60c6e815fe176207d7d1ceda814aca1b9d4fd6b1
DuncanRMcKeith <duncanmckeith3@gmail.com>
2025-01-07 10:31:33 -0500

    Updates from Class

--- W1/w1d2-practice.py
+++ W1/w1d2-practice.py
@@ -1,4 +1,12 @@
-#W1D2 Practice
-#SSE 126: Intermidate python. 
+#W1D2 Practice:SE 116 Review
+#SSE 126: Intermidate python:
 #Duncan McKeith
 #1/7/2025
+
+#Program Prompt
+
+
+#Variable Dictionary
+
+
+#-------Main Code Below--------------
