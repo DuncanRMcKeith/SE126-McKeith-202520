616405adfb75a25a8b2fc074578cc4c81b6d6ebf
DuncanRMcKeith <drmckeith@email.neit.edu>
2025-01-07 08:35:10 -0500

    test.py

--- practice/test.py
+++ practice/test.py
@@ -1,2 +1,3 @@
 #This is a test server for all SE126 Work, This file will only be to test our work for VSC and GitHub
 #This test code is another test to show ive properlly pulled it.
+#Here is an even newer line of code to show i can properlly pull it
