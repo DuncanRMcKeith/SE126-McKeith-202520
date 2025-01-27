8ddc152d7554c88d29b55a53f79799ba62e911eb
DuncanRMcKeith <duncanmckeith3@gmail.com>
2025-01-26 23:25:37 -0500

    Lab#3:

---
+++ W3/McKeith_lab3.py
@@ -0,0 +1,26 @@
+#Duncan McKeith
+#1-26-2025 
+#SE 126: Intermidate Python
+#Lab 3
+
+
+
+#Program Prompt
+
+#Variable Prompt: 
+
+#MAIN CODE BELOW
+
+#imports
+import csv
+#Functions
+
+#CODE BELOW
+
+#First our lists variables
+non_eligable = []
+old_notregistered = []
+didnt_vote = []
+voted = []
+records_processed = []
+
