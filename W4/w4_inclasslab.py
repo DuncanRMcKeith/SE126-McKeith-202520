f3a8ea3a4f4beebe3fb949d3933d8070c605bd74
DuncanRMcKeith <drmckeith@email.neit.edu>
2025-01-27 11:09:39 -0500

    Update w4_inclasslab.py

--- W4/w4_inclasslab.py
+++ W4/w4_inclasslab.py
@@ -7,3 +7,29 @@
 #Program Prompt
 
 #Variable Dictionary
+
+
+#--------MAIN CODE BELOW-------------------
+#Import
+import csv
+#Functions
+
+
+#CODE BELOW
+#VARIABLE LISTS
+fName = []
+lName = []
+test1 = []
+test2 = []
+test3 = []
+
+
+
+with open("Txt_Files/class_grades.csv") as csvfile:
+    file = csv.reader(csvfile)
+    for rec in file: 
+        fName.append(rec[0])
+        lName.append(rec[1])
+        test1.append(rec[2])
+        test2.append(rec[3])
+        test3.append(rec[4])
