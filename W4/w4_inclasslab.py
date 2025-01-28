6d6bb924014e99138a45738328120b05b2e4b57d
DuncanRMcKeith <drmckeith@email.neit.edu>
2025-01-28 08:47:31 -0500

    w4d2_demo

--- W4/w4_inclasslab.py
+++ W4/w4_inclasslab.py
@@ -13,8 +13,22 @@
 #Import
 import csv
 #Functions
-
-
+def letter (num):
+    '''This function is to take the average test scores then give back a corrisponding letter grade. '''
+    if num >= 90:
+        let = "A"
+    elif num >= 80:
+        let = "B"
+    elif num >= 70:
+        let = "C"
+    elif num >= 60:
+        let = "D"
+    elif num < 60:  
+        let = "F"
+    else:
+        let = "error"
+        
+    return let
 #CODE BELOW
 #VARIABLE LISTS
 fName = []
@@ -22,9 +36,10 @@ lName = []
 test1 = []
 test2 = []
 test3 = []
+num_avg = []
+lgrade = []
 
-
-
+#Data stored now to lists, still need to calculate avg amoungst test and letter grade
 with open("Txt_Files/class_grades.csv") as csvfile:
     file = csv.reader(csvfile)
     for rec in file: 
@@ -33,4 +48,21 @@ with open("Txt_Files/class_grades.csv") as csvfile:
         test1.append(rec[2])
         test2.append(rec[3])
         test3.append(rec[4])
-    
+#Disconnect #1 from main file        
+
+#Now part 1:  
+print(f"{'FirstName':8}   {'LastName':8}   {'test 1':3}   {'test 2':3}  {'test3':3}")
+for i in range(0,fName):
+    print(f"{fName[i]:8}   {lName[i]:8}   {test1[i]:3}   {test2[i]:3}  {test3[i]:3}")
+        
+        
+        
+        
+        
+
+
+for i in range (0,test1):
+    a = (test1[i] + test2[i] +test3[i])/3
+    num_avg.append(a)
+    l = letter(a)
+    lgrade.append(i)
