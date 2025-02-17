f6e630ce3f603c8c00d70264225c1e9d2dce5a77
DuncanRMcKeith <duncanmckeith3@gmail.com>
2025-02-17 13:09:32 -0500

    lab5

---
+++ W6/McKeith_Lab5.py
@@ -0,0 +1,25 @@
+#Duncan McKeith
+#2-17-2025
+#SE 126: Intermidiate Python
+#Lab 5
+
+
+#Program Prompt:
+
+
+#Variable Dictionary
+
+
+#--------------MAIN CODE BELOW-------------------------
+
+#imports
+import csv
+
+#Functions
+def menu():
+    '''This function is designed to handle the menu for the entire program, just to minimize the amount of indentation below. This program will return the option choice back to the program to sort and catagorize everything.'''
+    print("Library Options")
+
+#Executable Code
+
+#empty list variables from csv file book_list.csv 
