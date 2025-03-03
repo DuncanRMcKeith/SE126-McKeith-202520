168d79348b596ce47b7ec68ab9a815a49af8f46a
DuncanRMcKeith <drmckeith@email.neit.edu>
2025-03-03 09:21:17 -0500

    Update McKeith_Lab7.py

--- W8/McKeith_Lab7.py
+++ W8/McKeith_Lab7.py
@@ -4,33 +4,19 @@
 #Lab 7
 
 
-#Program Prompt:
+#Program Prompt:Build a mini programming dictionary a user can search through and ad to using the words.csv file, along with a menu to show, search, and add words to the dictionary as well as exit the program
 
 #Variable Dictionary:
-#
+#ans = while loop key for main executing code
+# opt = menu_opt
+# menu_opt = user input for menu choice, can only be the values 1-4, located within the menu function
+# found = Done for seqential search = If found if not found
+# search = user input for what word they are searching for
+# word_add = What word are you adding to the dictionary
+# bad = a flag used to check to see if the word is already within the dictionary, If true then the rest of the code does not execute
+# definition = what is the definition for the word_add chosen by the user, then checked twice to make sure the user isnt adding junk(Since we didnt make a remove function)
 # 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
+
 
 
 #----------------------MAIN CODE BELOW-------------------------------------
@@ -80,7 +66,7 @@ while ans == "y":
             print(f"{key:20}:\n\t{words[key]}")
         print('-'*50)
     elif opt == "2":
-        print("search for a word")
+        #Simple sequential Seach function dont need to overcomplicate it. 
         found = 0
         search = input("Please enter the word your searching for: ")
         for key in words:
@@ -114,7 +100,13 @@ while ans == "y":
             if definition_key == "n":
                 definition = str(input(f"What is the Definition for {word_add}?"))
         #Now i need to figure out how to append something to this! Cause setting it up like i did above except now with the terms word_add and definition doesnt work. 
-            # words.update({word_add} : {definition})------ISNT WORKING, cant tell why its not considering its like the exact same 
+            words.update({word_add : definition})#)------ISNT WORKING, cant tell why its not considering its like the exact same 
+            
+            #Note my issue was a single bracket of curlbraces.... Not 2.
             
     elif opt == "4": #Funny enough this is always the first code i add to my loops even before i make a menu functions. WHEN YOU MAKE A LOOP YOU SURE HAVE A WAY TO CLOSE IT
         ans = "n"
+        
+        
+print("Thanks for Using Your Python word Dictonary")
+print("Have a supercalafragalisticexpealidocious day!")
