3e64374082ff61951c2439f0b039f1439ecf3458
DuncanRMcKeith <duncanmckeith3@gmail.com>
2025-03-09 12:17:28 -0400

    Update McKeith_Final.py

--- Final Project/McKeith_Final.py
+++ Final Project/McKeith_Final.py
@@ -13,7 +13,8 @@
 import csv
 import os
 
-
+from os import system, name 
+from time import sleep 
 #-----FUNCTIONS-------------------------------------------
 def menu(fun):
     '''This is a basic menu function, that will handle the print statments and return the choice back to the main program.'''
@@ -22,31 +23,46 @@ def menu(fun):
     if fun == 0:
         print("\t\tWelcome Adventurer! To the Arcanum Of NOTES!")
         print("This magical index will allow you to keep track of details along you travel, this way you may stop asking the gods of this realm who or what is happening.")
-        fun +=1
+        fun += 1 #Okay so we need to change this on both a global scale and a non global scale
     else:
-        print("\t\tWelcome Back advenuture!")
+        print("Welcome Back Adventurer")
+  
     
     print("1.Show All Records")
     print("2.Search By Name")
     print("3.Search By Organization")
     print("4.Search by Last know Location")
     print("5.Add an entry")
-    print("6.Seal the Index.... for now")
-    menu_choice = input("Please Select from The Index")
-    return fun, menu_choice
+    print("6.Edit an entry")
+    print("7.Seal the Index.... for now")
+    menu_choice = input("Please Select from The Index [1-7]")
+    while menu_choice not in ['1','2','3','4','5','6','7']:
+        print("Ah... please select a proper entry")
+        menu_choice = input("Please Select from The Index [1-7]")
+    return menu_choice 
+# import only system from os 
 
-
+  
+# define our clear function 
+def clear(): 
+  
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+  
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear') 
 
 
 #-----EXECUTING CODE BELOW--------------------------------
-name = []
+names = []
 org = []
 location = [] #Note this stands for last location not current location as npcs and people can move,
 notes = []
-fun = 0 
 ans = "y"
 total_rec = 0
-
+fun = 0
 # checks for file, if not found, generates new list:
 if not os.path.exists('Final Project/dndnotes.csv'):
     print("\n No Notes File Found. Generating New Notepad Adventurer!")
@@ -76,11 +92,59 @@ with open ("Final Project/dndnotes.csv", 'r') as csvfile:
     file = csv.reader(csvfile)
 
     for rec in file:
-        name.append(rec[0])
+        names.append(rec[0])
         org.append(rec[1])
         location.append(rec[2])
         notes.append(rec[3])
 #Disconnect now from file, from here on out we are in our menu and will be working within a while loop
 while ans == "y":
-    menu(fun)
-    
+    choice = menu(fun)
+    fun += 1 #Okay so this the first time the code will run will read as 0 From there on out the value will change to not be 0
+    if choice == "1":
+        print("Show All Records")#Just show all the answers
+        print(f"{'Name':20} {'Organization':25} {'Last Location'}")
+        print("-"*60)
+        for i in range(0,len(names)):
+            print(f"{names[i]:20} {org[i]:25} {location[i]}")
+
+    if choice == "2":
+        print("Search by Name") #Simple sequential search
+        found = -1
+        search = input("Please Write the Name your searching for").lower()
+        for i in range(0,len(names)):
+            if search.lower() == names[i].lower():
+                found = i
+        if found != -1:
+            print(f"{names} entry was found")
+            print(f"{'Name':20} {'Organization':25} {'Last Location'}")
+            print("-"*60)
+            print(f"{names[found]:20} {org[found]:25} {location[found]}")
+            print(f"Notes: {notes[found]}")
+        else:
+            print(f"Apologies There are no Entries for {search}\n Please Try again or add a new entry")
+
+
+    if choice == "3":
+        print("Search by Organization")
+        found = [] #Can be multiple within an organization, so its a sequential search again but this time now a list
+        search = input("Please Write the organization your searching for").lower()
+        for i in range(0,len(org)):
+            if search.lower() == org[i].lower():
+                found.append(i)
+        if not found:
+            print(f"Apologies your search for {search} was not found")
+        else:
+            print(f"Your search for ")
+            for i in range(0,(found)):
+                print()
+    if choice == "4":
+        print("Search by Last Known Location")    
+    if choice == "5":
+        print("5.Add an entry")
+    if choice == "6":
+        print("6.Edit an entry")
+    if choice == "7":
+        print("Close the index")
+        ans = "n"
+    input("Press Enter to Continue")
+    clear()
